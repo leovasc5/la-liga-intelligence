195098430f5822602ae156c7bf3aff7cf10f25c3
leovasc5 <leovasc5@hotmail.com>
2021-07-20 14:07:35 -0300

    Iniciando a estruturação dos dados

--- controllers/atualizarBD.py
+++ controllers/atualizarBD.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import os
+import os
+from pathlib import Path
+p = Path(os.getcwd())
+
+df = pd.read_csv(str(p)+"\\database\\dados.csv", encoding="UTF-8", sep=";")
+print(df.head())
