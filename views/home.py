e98d260c5cb8f95fd7cae971fe869340a8be38ab
leovasc5 <leovasc5@hotmail.com>
2021-07-21 13:57:34 -0300

    Iniciando função de gráficos

--- views/home.py
+++ views/home.py
@@ -1,9 +1,14 @@
-import os
+import os, sys, inspect
 from pathlib import Path
 from tkinter import *
 from tkinter import ttk
-import numpy as np
-from tkinter import messagebox
+# import numpy as np
+# from tkinter import messagebox
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+from graphics.main import *
 
 p = Path(os.getcwd())
 
@@ -44,15 +49,29 @@ class App:
             tv.insert("", "end",values=(str(posicao[i-1]), nome[i-1], str(pontos[i-1]), str(partidas[i-1]), str(vitorias[i-1]), str(empates[i-1]), str(derrotas[i-1]), 
             str(gp[i-1]), str(gc[i-1]), str(saldo[i-1]), str(ca[i-1]), str(cv[i-1])))
 
-        frameGraficos = LabelFrame(root, text="Gráficos")
-        frameGraficos.place(x=30, y=575)
+        frameGraficos = LabelFrame(root, text="Gráficos", padx=10, pady=10, font="Helvetica 16 bold")
+        frameGraficos.place(x=30, y=575, width=915)
 
-        lbl2 = Label(frameGraficos, text="Telefone: ", padx=10, pady=10)
-        lbl2.pack(side="left")
-        tlf = Entry(frameGraficos)
-        tlf.pack(side="left", padx=10)
+        btn2 = Button(frameGraficos, text="Pontuação (Barras)", command=lambda: self.call("pt_ba"))
+        btn2.pack(side="left", padx=10, pady=10)
 
-        btn1 = Button(frameGraficos, text="Gráfico da pontuação")
-        btn1.place(x=400, y=400)
+        btn3 = Button(frameGraficos, text="Pontuação (Pizza)")
+        btn3.pack(side="left", padx=10, pady=10)
+
+        btn4 = Button(frameGraficos, text="Pontuação (Histograma)")
+        btn4.pack(side="left", padx=10, pady=10)
+
+        btn5 = Button(frameGraficos, text="Gols (Barras)")
+        btn5.pack(side="left", padx=10, pady=10)
+        
+        btn6 = Button(frameGraficos, text="Gols (Pizza)")
+        btn6.pack(side="left", padx=10, pady=10)
+
+        btn4 = Button(frameGraficos, text="Gols (Histograma)")
+        btn4.pack(side="left", padx=10, pady=10)
 
         root.mainloop()
+
+    def call(self, modo):
+        pt_ba() if modo == "pt_ba" else None
+
