3ba9cddd0994107ab93c3e8fe25bcf63c3ea8391
leovasc5 <leovasc5@hotmail.com>
2021-07-27 12:08:42 -0300

    Iniciando modulação de PDFs

--- reports/pontuacao.py
+++ reports/pontuacao.py
@@ -1,5 +1,20 @@
+import os, sys, inspect
+from pathlib import Path
 from reportlab import *
+from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
-from reportlab.platypus import SimpleDocTemplate, Table
 
-doc = SimpleDocTemplate(os.path.dirname(__file__)+"\\contatos.pdf", pagesize=A4)
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+p = Path(os.getcwd())
+caminho = str(p)+"\\assets\\dataFiles\\"
+print(type(caminho))
+cnv = canvas.Canvas(caminho+"relatorio_pontuacao.pdf", pagesize=A4)
+cnv.drawString(50, 50, "Leonardo Vasconcelos")
+cnv.setTitle("Relatório de Pontuação da La Liga")
+cnv.save()
+cnv.showPage()
+print("PDF Criado")
+# doc.build()
