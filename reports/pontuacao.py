96d1edd80eadb5446f470133d2ece0391c28ac42
leovasc5 <leovasc5@hotmail.com>
2021-07-28 10:12:28 -0300

    Criado padrão estético do PDF

--- reports/pontuacao.py
+++ reports/pontuacao.py
@@ -4,17 +4,33 @@ from reportlab import *
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
+
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 p = Path(os.getcwd())
 caminho = str(p)+"\\assets\\dataFiles\\"
-print(type(caminho))
+print(p)
 cnv = canvas.Canvas(caminho+"relatorio_pontuacao.pdf", pagesize=A4)
-cnv.drawString(50, 50, "Leonardo Vasconcelos")
+cnv.drawImage(str(p)+"\\assets\\img\\logo.png", x=50, y=750, width=150, height=50, mask="auto")
+
+cnv.setFont('Helvetica-Bold', 12)
+cnv.drawString(260, 75, "Desenvolvido por: ")
+cnv.setFont('Helvetica', 12)
+cnv.drawString(370, 75, "Leonardo Vasconcelos Paulino")
 cnv.setTitle("Relatório de Pontuação da La Liga")
+
+cnv.setFont('Helvetica-Bold', 12)
+cnv.drawString(260, 50, "GitHub: ")
+cnv.setFont('Helvetica', 12)
+cnv.drawString(308, 50, "www.github.com/leovasc5")
+cnv.linkURL("https://github.com/leovasc5",(300,50))
+
+
+
+# cnv.setTitle("Relatório de Pontuação da La Liga")
+
 cnv.save()
 cnv.showPage()
 print("PDF Criado")
-# doc.build()
